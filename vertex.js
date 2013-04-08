fed23f93ce4805d86f879ca65541232f44156de8
Michael Schilonka <michael@schilonka.de>
2013-04-08 11:06:17 +0200

    changed class names and formatr

--- vertex.js
+++ vertex.js
@@ -1,16 +1,15 @@
-
-			var vertex = new Class({
-				initialize : function(myTitle, myIntro, myLink, myLevel,myParent) {
-					this.id = 0;
-					this.parent = myParent;
-					this.ajax
-					this.children = new Array();
-					this.title = myTitle;
-					this.intro = myIntro;
-					this.link = myLink;
-					this.outlinks;
-					this.svg;
-					this.level = myLevel;
-					this.path;
-				},
-			});
+var Vertex = new Class({
+	initialize : function(myTitle, myIntro, myLink, myLevel, myParent) {
+		this.id = 0;
+		this.parent = myParent;
+		this.ajax
+		this.children = new Array();
+		this.title = myTitle;
+		this.intro = myIntro;
+		this.link = myLink;
+		this.outlinks;
+		this.svg;
+		this.level = myLevel;
+		this.path;
+	},
+});
