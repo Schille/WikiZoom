79a7a11c7513a2c2a67c6832384bdeb60a85943a
Robert Stein <roddirockz@googlemail.com>
2013-04-15 22:24:57 +0200

    some refactoring + code documentation

--- vertex.js
+++ vertex.js
@@ -1,16 +1,16 @@
 var Vertex = new Class({
 	initialize : function() {
-		this.id;
-		this.parent;
-		this.ajax
-		this.children = new Array();
-		this.title;
-		this.intro;
-		this.link;
-		this.outlinks = new Array();;
-		this.svg;
-		this.level;
-		this.path;
+		this.id;						//unique id of vertex {Number}
+		this.parent;					//parent vertex {Vertex}
+		this.ajax						//AJAX request object
+		this.children = new Array();	//array of vertex children [{Vertex}]
+		this.title;						//title of vertex {String}
+		this.intro;						//parsed intro text {String}
+		this.link;						//link to article on wikipedia {String}
+		this.outlinks = new Array();;   //array of title from outgoing links [{String}]
+		this.svg;						//SVG object of vertex {Raphael}
+		this.level;						//level of vertex in tree structure {Number}
+		this.path;						//SVG object of vertex to it parent {Raphael}
 	},
 
 }); 
